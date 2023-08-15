cf66e99e44a616fd17d494e683528efdbbaa05f4
Jesper <jesper.92.nilsson@gmail.com>
2023-08-15 10:50:52 +0200

    final changes for Contactpage

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -61,8 +61,8 @@ function aboutUs() {
       </div>
 
       {/* History Div */}
-      <div className="flex flex-col justify-center items-center  bg-green-300 text-xl px-24">
-        <h1 className="text-2xl font-bold text-comfy-blue underline decoration-light-comfy-red decoration-4 p-2  bg-green-200 w-2/3">
+      <div className="flex flex-col justify-center items-center text-xl px-24">
+        <h1 className="text-2xl font-bold text-comfy-blue underline decoration-light-comfy-red decoration-4 p-2 w-2/3">
           Historia
         </h1>
         <p className="w-2/3 p-2">
